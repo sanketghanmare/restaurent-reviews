874070bdec677461aab5b11a18f797ed258f0dc6
Sanket Ghanmare <sanket@Sankets-iMac.local>
2021-06-16 16:38:39 -0400

    added index.js and api

---
+++ backend/index.js
@@ -0,0 +1,24 @@
+import app from "./server"
+import mongodb from "mongodb"
+import dotenv from "dotenv"
+
+dotenv.config();
+const MongoClient = mongodb.MongoClient
+
+const port = process.env.PORT || 8000;
+
+MongoClient.connect(
+    process.env.RESTREVIEWS_DB_URI,
+    {
+        poolSize: 50,
+        wtimeout: 2500,
+        useNewUrlParse: true
+    }
+).catch(err => {
+    console.error(err.stack);
+    process.exit(1);
+}).then(async client => {
+    app.listen(port, () => {
+        console.log(`Listening on port ${port}`);
+    })
+})

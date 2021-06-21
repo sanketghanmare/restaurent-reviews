d977d50ad4bcbd196e7d8da309961d9a8d346df1
Sanket Ghanmare <sanket@Sankets-iMac.local>
2021-06-21 12:14:53 -0400

    make backend server less

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
 import app from "./server.js"
 import mongodb from "mongodb"
 import dotenv from "dotenv"
+import RestaurantsDAO from "./dao/restaurantsDAO.js";
+import ReviewsDAO from "./dao/reviewsDAO.js";
 
 dotenv.config();
 const MongoClient = mongodb.MongoClient
@@ -18,6 +20,9 @@ MongoClient.connect(
     console.error(err.stack);
     process.exit(1);
 }).then(async client => {
+    await RestaurantsDAO.injectDB(client)
+    await ReviewsDAO.injectDB(client)
+
     app.listen(port, () => {
         console.log(`Listening on port ${port}`);
     })
